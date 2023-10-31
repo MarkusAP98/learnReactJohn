38cd8c9eeccea03cfcc77fa3c5ab12481a54b33d
Markus Phantha <markus.a.phantha@gmail.com>
2023-10-31 14:43:01 +0200

    components

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,30 @@
 function ListGroup() {
-    const items = [
-        'New York',
-        'Los Angeles',
-        'Chicago',
-        'Houston',
-        'Phoenix',
-        'Philadelphia',
-        'San Antonio',
-        'San Diego',
-    ];
-    return (
-        <>
-        <h1>ListGroup</h1>
-        <ul className="list-group">
-        {items.map(item => <li className="list-group-item">{item}</li>)}
-        </ul>
-      </>
-    );
- 
+  let items = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+    "Philadelphia",
+    "San Antonio",
+    "San Diego",
+  ];
+
+  items = [];
+
+
+  return (
+    <>
+      <h1>ListGroup</h1>
+      <ul className="list-group">
+        {items.map((item) => (
+          <li key={item} className="list-group-item">
+            {item}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default ListGroup;
