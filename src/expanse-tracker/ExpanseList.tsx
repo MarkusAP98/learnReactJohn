68c9a93b32bd94a8feda49f9fee5d783a9708e9a
Markus Phantha <markus.a.phantha@gmail.com>
2023-11-02 13:37:17 +0200

    init mosh solutions

---
+++ src/expanse-tracker/ExpanseList.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+interface Expense {
+    id: number;
+    description: string;
+    amount: number;
+    category: string;
+}
+
+interface Props {
+    expanses: Expense[]
+    onDelete: (id: number) => void;
+}
+
+const ExpanseList = ({ expenses }: Props) => {
+  return (
+    <table className="table table-bordered">
+        <thead>
+            <tr>
+                <th>Description</th>
+                <th>Amount</th>
+                <th>Category</th>
+                <th></th>
+            </tr>
+        </thead>
+        <tbody>
+        {expenses.map(expense => <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+                <button className="btn btn-outline-danger" onClick={() => onDelete(expense.id)}>Delete</button>
+            </td>
+        </tr>)}
+        </tbody>
+    </table>
+  )
+}
+
+export default ExpanseList
