2e2c102e8f9ae1db9e8e71b912f8468357170cbe
Markus Phantha <markus.a.phantha@gmail.com>
2023-11-02 14:49:35 +0200

    the ExpenseList

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import { useState } from "react";
-import Shopping from "./components/Shopping";
+import ExpanseList from "./expanse-tracker/ExpanseList";
+// import Shopping from "./components/Shopping";
 // import Form from "./components/Form";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    {
+      id: 1,
+      description: "aaa",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 2,
+      description: "bbbb",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 3,
+      description: "cccc",
+      amount: 10,
+      category: "Utilities",
+    },
+    {
+      id: 41,
+      description: "DDdd",
+      amount: 10,
+      category: "Utilities",
+    },
+  ]);
 
-  return <div className="">
-    <Shopping />
-    {/* <Form /> */}
-  </div>;
-} 
-
+  return (
+    <div className="">
+      {/* <Shopping /> */}
+      {/* <Form /> */}
+      <ExpanseList
+        expenses={expenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
+}
 
 export default App;
