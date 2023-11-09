9f9bc386cbe75c0e3f4f1bdb8bb25b20b3ee8596
Markus Phantha <markus.a.phantha@gmail.com>
2023-11-09 11:57:43 +0200

    Add expense tracker components and functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,35 @@
 import { useState } from "react";
+import ExpanseList from "./components/expanse-tracker/component/ExpanseList";
+import ExpenseFilter from "./components/expanse-tracker/component/ExpenseFilter";
 
-function App() {
-  return <div></div>;
+function App({}) {
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [expenses, SetExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Utilities" },
+    { id: 2, description: "bbb", amount: 15, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 5, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 20, category: "Utilities" },
+  ]);
+
+  const visibleExpenses = selectedCategory
+    ? expenses.filter((e) => e.category === selectedCategory)
+    : expenses;
+
+  return (
+    <div>
+      <div className="md-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+        <ExpanseList
+          expenses={visibleExpenses}
+          onDelete={(id) =>
+            SetExpenses(expenses.filter((expense) => expense.id !== id))
+          }
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
